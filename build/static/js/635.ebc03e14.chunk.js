1b1641e82b80c31f6d8d7b63a5d9faf76edd752b
Andrey999K <52581965+Andrey999K@users.noreply.github.com>
2023-06-23 16:41:45 +0300

    Delete build directory

--- build/static/js/635.ebc03e14.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkemilus=self.webpackChunkemilus||[]).push([[635],{50635:function(e,u,n){n.r(u),n.d(u,{DefaultDashboard:function(){return a}});n(72791);var r=n(80184),a=function(){return(0,r.jsx)(r.Fragment,{children:"Dashboard"})};u.default=a}}]);
-//# sourceMappingURL=635.ebc03e14.chunk.js.map
